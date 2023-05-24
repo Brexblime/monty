a73184dfba66b3d0b216c0aead5e1c8be9ab2558
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-24 20:10:48 +0100

    Implement the nop opcode.
    The nop opcode
    
    The opcode nop doesn’t do anything.
    
    Usage: nop

--- execute.c
+++ execute.c
@@ -14,6 +14,7 @@ int execute(char *line, stack_t **stack, unsigned int line_number,
 	instruction_t opcodes[] = {
 		{"push", push}, {"pall", pall},{"pint", pint},
 		{"pop", pop},{"swap", swap},{"add", add},
+		{"nop", nop},
 		{"stack", stack_c}, {"queue", queue_c},
 		{NULL, NULL},
 	};
