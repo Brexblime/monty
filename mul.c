47bc3f6820362e79061067a8748239136d45c747
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-25 18:59:51 +0100

    Implement the mul opcode.
    The mul opcode
    
    The opcode mul multiplies the second top element of the stack with the top element of the stack.

--- mul.c
+++ mul.c
@@ -1,24 +1,18 @@
 #include "monty.h"
 
 /**
- * mul - mults the second top element of the stack with the top element
- * @stack: double pointer to the top of the stack
- * @line_number: line number being executed from the monty file
+ * mul - multiplies the second top element of stack with top element of the stack
+ * @stack: pointer to the stack
+ * @line_number: current line number
 */
 void mul(stack_t **stack, unsigned int line_number)
 {
-	int a, b;
-
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	a = (*stack)->n;
-	b = (*stack)->next->n;
-
-	(*stack)->next->n = a * b;
-
+	(*stack)->next->n *= (*stack)->n;
 	pop(stack, line_number);
 }
